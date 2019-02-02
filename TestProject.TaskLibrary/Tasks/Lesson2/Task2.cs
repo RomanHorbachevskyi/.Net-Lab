cb5a035a2f054e8644835b87d27f5f2587fab52c
RomanHo <gororogen@gmail.com>
2019-02-02 19:18:21 +0200

    forgot to add files before previous commit

---
+++ TestProject.TaskLibrary/Tasks/Lesson2/Task2.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson2
+{
+    public class Task2 : IRunnable
+    {
+        public void Run() // main method
+        {
+            Console.WriteLine("*** Now you are in Lesson2.Task2 ***");
+            //Console.WriteLine("*** Select one of Tasks listed below ***");
+            Console.WriteLine("\n* Task2: Calculate 'Perimetr' and 'Area' of rectangle using Auto-implemented Properties" +
+                              "\n       Needed Top-Left and Bottom-Right coordinates");
+
+            
+            // initializing Coordinates
+            int TLX;
+            int TLY;
+            int BRX;
+            int BRY;
+
+            NextTry:
+            TLX = 0;
+            TLY = 0;
+            BRX = 0;
+            BRY = 0;
+
+            //setting coordinates for Rectangle
+            Console.WriteLine("Enter Top-Left coordinate (int) X: ");
+            string s = Console.ReadLine().ToLower();
+            this.SetCd(ref s, ref TLX);
+            Console.WriteLine("Enter Top-Left coordinate (int) Y: ");
+            s = Console.ReadLine().ToLower();
+            this.SetCd(ref s, ref TLY);
+            Console.WriteLine("Enter Bottom-Right coordinate (int) X: ");
+            s = Console.ReadLine().ToLower();
+            this.SetCd(ref s, ref BRX);
+            Console.WriteLine("Enter Bottom-Right coordinate (int) Y: ");
+            s = Console.ReadLine().ToLower();
+            this.SetCd(ref s, ref BRY);
+
+            //ckecking for Zero length
+            if ((TLX == BRX) | (TLY == BRY))
+            {
+                Console.WriteLine("Wrong coordinates. Some side of rectangle has 0 length.");
+                goto NextTry;
+            }
+
+            //creating Rectangle instance
+            Rectangle2 rec=new Rectangle2(ref TLX,ref TLY,ref BRX,ref BRY);
+            rec.Perimeter();
+            rec.Area();
+        }
+
+        public void SetCd(ref string S, ref int Cd)
+        {
+            //setting Coordinates or Exit
+            if ((S == "q") | (S == "b"))
+            {
+                Environment.Exit(0);
+            }
+            //bool parsed= Int32.TryParse(s, out TLX);
+            while (!Int32.TryParse(S, out Cd))
+            {
+                Console.WriteLine("Entered incorrect value.\nEnter only digits: ");
+                S = Console.ReadLine().ToLower();
+                this.SetCd(ref S,ref Cd);
+            }
+        }
+
+    }
+
+    public class Rectangle2
+    {
+        //auto-implemented properties
+        public int TLX { get; set; }
+        public int TLY { get; set; }
+        public int BRX { get; set; }
+        public int BRY { get; set; }
+
+
+        //(int tlx, int tly, int brx, int bry)
+        //private int TLX, TLY, BRX, BRY;
+
+        public Rectangle2(ref int tlx, ref int tly, ref int brx, ref int bry)
+        {
+            TLX = tlx;
+            TLY = tly;
+            BRX = brx;
+            BRY = bry;
+        }
+
+        public void Perimeter()
+        {
+            int a = BRX - TLX;
+            if (a < 0)
+                a *= -1;
+            int b = TLY - BRY;
+            if (b < 0)
+                b *= -1;
+            Console.WriteLine("Rectangle with coordinates Top-Left X, Y: {0}, {1}",TLX,TLY);
+            Console.WriteLine("and Bottom-Right X, Y: {0}, {1}", BRX, BRY);
+            Console.WriteLine("has Perimeter: "+ (2 * (a + b)));
+            Console.WriteLine();
+
+        }
+
+        public void Area()
+        {
+            int a = BRX - TLX;
+            if (a < 0)
+                a *= -1;
+            int b = TLY - BRY;
+            if (b < 0)
+                b *= -1;
+            Console.WriteLine("Rectangle with coordinates Top-Left X, Y: {0}, {1}", TLX, TLY);
+            Console.WriteLine("and Bottom-Right X, Y: {0}, {1}", BRX, BRY);
+            Console.WriteLine("has Area: " + (a * b));
+            Console.WriteLine();
+        }
+    }
+
+}
---
+++ TestProject.TaskLibrary/Tasks/Lesson2/Task3.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson2
+{
+    public class Task3 : IRunnable
+    {   //calculate circle length and area
+
+        public void Run() // main method
+        {
+            Console.WriteLine("*** Now you are in Lesson2.Task3 ***");
+            Console.WriteLine("\n* Task3: Calculate 'Length' and 'Area' of circle using methods" +
+                              "\n       Needed Radius");
+
+            int R = 0;
+
+            NextTry:
+            
+            //setting Radius for Circle
+            Console.WriteLine("Enter Radius (int): ");
+            string s = Console.ReadLine().ToLower();
+            this.SetR( s);
+
+            R=Int32.Parse(s);
+            //ckecking for Zero length
+            if (R<=0)
+            {
+                Console.WriteLine("Wrong Radius.");
+                goto NextTry;
+            }
+
+            //creating Rectangle instance
+            Circle crl=new Circle(ref R);
+            crl.Length(ref R);
+            crl.Area(ref R);
+        }
+
+        private void SetR(object R)
+        {
+            int r;
+            //setting Radius or Exit
+            if ((R.ToString() == "q") | (R.ToString() == "b"))
+            {
+                Environment.Exit(0);
+            }
+            //bool parsed= Int32.TryParse(R, out R);
+            while (!Int32.TryParse(R.ToString(), out r) | (r <= 0))
+            {
+                Console.WriteLine("Entered incorrect value.\nEnter only positive digits: ");
+                R = Console.ReadLine().ToLower();
+                this.SetR(R);
+            }
+        }
+
+    }
+
+    public class Circle
+    {
+        //constants
+        private const double pi = 3.14;
+
+        //auto-implemented properties
+        public int R { get; set; }
+        
+        //constructor
+        public Circle(ref int r)
+        {
+            R = r;
+        }
+        public void Length(ref int r)
+        {
+            R = r;
+            Console.WriteLine("Circle with Radius={0} has Length=" + (2 * pi * r), r);
+            Console.WriteLine();
+        }
+        
+        public void Area(ref int r)
+        {
+            R = r;
+            Console.WriteLine("Circle with Radius={0} has Area=" + (pi * r*r), r);
+            Console.WriteLine();
+        }
+    }
+
+}
