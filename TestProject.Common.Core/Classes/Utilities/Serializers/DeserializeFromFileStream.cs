0a1aabe006dfce03459db127d43e9ae73806ccd7
RomanHorbachevskyi <gororogen@gmail.com>
2019-04-04 09:54:31 +0300

    Added Lesson13, SafeFileEnumerator, Serializers

---
+++ TestProject.Common.Core/Classes/Utilities/Serializers/DeserializeFromFileStream.cs
@@ -0,0 +1,79 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace TestProject.Common.Core.Classes.Utilities.Serializers
+{
+    /// <summary>
+    /// Derializers to deserialize object from FileStream
+    /// </summary>
+    public static class DeserializeFromFileStream
+    {
+        /// <summary>
+        /// Deserializes specified object from Binary format.
+        /// </summary>
+        /// <typeparam name="T">Type of resulting object.</typeparam>
+        /// <param name="file">File name with or w/o path.</param>
+        /// <param name="obj">Object to serialize.</param>
+        /// <param name="fMode">File mode.</param>
+        public static void DeserializeBinary<T>(ref string file, out T obj, FileMode fMode)
+        {
+            using (FileStream fs = new FileStream(file, FileMode.Open))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                obj = (T) bf.Deserialize(fs);
+            }
+        }
+
+        public static void DeserializeBinary<T>(ref string file, out T[] obj, FileMode fMode)
+        {
+            using (FileStream fs = new FileStream(file, FileMode.Open))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                obj = (T[])bf.Deserialize(fs);
+                //T[] carsB = (T[])bf.Deserialize(fs);
+                //T[] carsBin = new T[carsB.Length]; //new Car[carsB.Count];
+                //obj = carsBin;// = carsB; //carsB.CopyTo(carsBin);
+                //ConsIO.WriteLine($"Deserialized carsBin has {carsBin.Length} instances.");
+            }
+        }
+
+        /// <summary>
+        /// Deserializes specified object from JSON format.
+        /// </summary>
+        /// <typeparam name="T">Type of resulting object.</typeparam>
+        /// <param name="file">File name with or w/o path.</param>
+        /// <param name="obj">Object to serialize.</param>
+        /// <param name="fMode">File mode.</param>
+        public static void DeserializeJSON<T>(ref string file, out T obj, FileMode fMode)
+        {
+            using (FileStream fs = new FileStream(file, fMode))
+            {
+                TextReader tr = new StreamReader(fs);
+                obj = JsonConvert.DeserializeObject<T>(tr.ReadToEnd());//, Formatting.Indented);
+            }
+        }
+
+        /// <summary>
+        /// Deserializes specified object from XML format.
+        /// </summary>
+        /// <typeparam name="T">Type of resulting object.</typeparam>
+        /// <param name="file">File name with or w/o path.</param>
+        /// <param name="obj">Object to serialize.</param>
+        /// <param name="fMode">File mode.</param>
+        public static void DeserializeXML<T>(ref string file, out T obj, FileMode fMode)
+        {
+            using (FileStream fs = new FileStream(file, fMode))
+            {
+                XmlSerializer xS = new XmlSerializer(typeof(T));
+                obj = (T)xS.Deserialize(fs);
+            }
+        }
+    }
+}
---
+++ TestProject.Common.Core/Classes/Utilities/Serializers/SerializeToFileStream.cs
@@ -0,0 +1,104 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace TestProject.Common.Core.Classes.Utilities.Serializers
+{
+    /// <summary>
+    /// Serializers to serialize object to File
+    /// </summary>
+    public static class SerializeToFileStream
+    {
+        /// <summary>
+        /// Serializes specified object to the specified format
+        /// </summary>
+        /// <param name="file">File name with or w/o path.</param>
+        /// <param name="obj">Object to serialize.</param>
+        /// <param name="fMode">File mode.</param>
+        /// <param name="convert">Format to convert to.</param>
+        public static void Serialize(ref string file, object obj, FileMode fMode, ConvertFromTo convert)
+        {
+            Type type = obj.GetType();
+            switch (convert)
+            {
+                case ConvertFromTo.Binary:
+                {
+                        SerializeBinary(ref file, obj, fMode);
+                    break;
+                }
+                case ConvertFromTo.JSON:
+                {
+                        SerializeJSON(ref file, obj, fMode);
+                    break;
+                }
+                case ConvertFromTo.XML:
+                {
+                        SerializeXML(ref file, obj, fMode);
+                    break;
+                }
+                default:
+                {
+
+                    break;
+                }
+            }
+            
+        }
+
+        /// <summary>
+        /// Serializes specified object into Binary format.
+        /// </summary>
+        /// <param name="file">File name with or w/o path.</param>
+        /// <param name="obj">Object to serialize.</param>
+        /// <param name="fMode">File mode.</param>
+        public static void SerializeBinary(ref string file, object obj, FileMode fMode)
+        {
+            using (FileStream fs = new FileStream(file, fMode))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fs, obj);
+            }
+        }
+
+        /// <summary>
+        /// Serializes specified object into JSON format.
+        /// </summary>
+        /// <param name="file">File name with or w/o path.</param>
+        /// <param name="obj">Object to serialize.</param>
+        /// <param name="fMode">File mode.</param>
+        public static void SerializeJSON(ref string file, object obj, FileMode fMode)
+        {
+            using (StreamWriter sw = File.CreateText(file))
+            //using (FileStream fs = new FileStream(file, fMode))
+            {
+                //FileStream sw = File.OpenWrite(file);
+                //StreamWriter sw = File.CreateText(file);
+                
+                string jData = JsonConvert.SerializeObject(obj);//, Formatting.Indented);
+                //ConsIO.WriteLine(jData);
+                sw.WriteLine(jData);
+            }
+        }
+        
+        /// <summary>
+        /// Serializes specified object into XML format.
+        /// </summary>
+        /// <param name="file">File name with or w/o path.</param>
+        /// <param name="obj">Object to serialize.</param>
+        /// <param name="fMode">File mode.</param>
+        public static void SerializeXML(ref string file, object obj, FileMode fMode)
+        {
+            using (FileStream fs = new FileStream(file, fMode))
+            {
+                XmlSerializer xS = new XmlSerializer(obj.GetType());
+                xS.Serialize(fs, obj);
+            }
+        }
+    }
+}
